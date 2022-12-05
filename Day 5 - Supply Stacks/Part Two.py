5755c259973867ec420fc9cd5892b750ad71ec51
EricLoBin <ekilobin@gmail.com>
2022-12-05 10:42:38 -0300

    [ADD] Day 5.2

---
+++ Day 5 - Supply Stacks/Part Two.py
@@ -0,0 +1,33 @@
+from inputs.input import get_input
+
+inp = get_input(5).read().split("\n\n")
+
+
+lines_crates = inp[0].split("\n")
+
+piles = {lines_crates[-1][i]: [] for i in range(1, len(lines_crates[0]), 4)}
+
+for i in range(1, len(lines_crates[0]), 4):
+    for line in reversed(lines_crates):
+        if (
+            line[i] == " "
+            or not line[i].isalpha()
+        ):
+            continue
+        piles[lines_crates[-1][i]].append(line[i])
+
+instructions = inp[1].split("\n")
+
+for instruction in instructions:
+    i = instruction.split(" ")
+    quatity = int(i[1])
+    pile = i[3]
+    destination = i[5]
+
+    piles[destination].extend(piles[pile][-quatity:])
+    piles[pile] = piles[pile][:-quatity]
+
+for pile in piles:
+    print(piles[pile].pop(), end="")
+
+print()
